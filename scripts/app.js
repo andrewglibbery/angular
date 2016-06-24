cd492c691238ac01290a3e7333ee6e26041f1505
Andrew Glibbery <andrew.glibbery@gmail.com>
2016-06-24 11:59:49 -0400

    adds first controller

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 angular.module("todoListApp", [])
-.controller('mainCtrl', function($scope) {
-  $scope.helloWorld = function() {
-   console.log("hello there");
-  };
+.controller('mainCtrl', function($scope){
+	$scope.helloWorld = function() {
+		console.log("Hello There! this is the helloWorld controller function, in the mainCtrl");
+	};
 });
